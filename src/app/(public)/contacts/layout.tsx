5a311d89306c7650d19651a6a1c6367effed9b81
aleks1303 <aleksbulda13@gmail.com>
2025-05-30 21:39:54 +0300

    router base

--- src/app/(public)/contacts/layout.tsx
+++ src/app/(public)/contacts/layout.tsx
@@ -4,6 +4,7 @@ import React from "react";
 export const metadata: Metadata = {
     title: "ContactsLayout metadata"
 };
+
 type Props = { children: React.ReactNode }
 const ContactsLayout = ({children}: Props) => {
     return (
