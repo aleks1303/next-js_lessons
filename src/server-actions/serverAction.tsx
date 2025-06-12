8187c72b1052ab19103124f768ac07553f512f84
aleks1303 <aleksbulda13@gmail.com>
2025-06-12 16:43:42 +0300

    homework create car

--- src/server-actions/serverAction.tsx
+++ src/server-actions/serverAction.tsx
@@ -10,23 +10,4 @@ export const formCreateCar = async (formData:FormData) => {
 
     await apiCars.createCar({brand, price, year})
     redirect('/cars')
-
 }
-
-
-
-// 'use server';
-//
-// import { apiCars } from "@/services/api.service";
-//
-// export const formCreateCar = async (formData: FormData) => {
-//     const brand = formData.get('brand') as string;
-//     const price = Number(formData.get('price'));
-//     const year = Number(formData.get('year'));
-//
-//     if (!brand || isNaN(price) || isNaN(year)) {
-//         throw new Error("Invalid input data");
-//     }
-//
-//     await apiCars.createCar({ brand, price, year });
-// };
