466f19b752652e78a5c6fb0f94da4d6c40989855
aleks1303 <aleksbulda13@gmail.com>
2025-06-23 15:51:30 +0300

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,46 @@
-// import {NextRequest} from "next/server";
+import {NextRequest} from "next/server";
+
+export const middleware = (request:NextRequest) => {
+    console.log('middleware');
+
+
+
+   // отримаємо токен з cookie, sessionStorage, localStorage,
+    // можна отримати інформацію яка локалізація, для того, щоб робити запити на певну url
+    //з інформацією необхідної мови
+
+
+    // const token = 'skdk4jfjdj3jdjdjj55866ej'
+    //
+    // const response = NextResponse.next({
+    //     headers: {
+    //         authorization: token
+    //     }
+    // });
+    // return response
 //
-// export const middleware = (request:NextRequest) => {}
+    // це для того, щоб дістати інфу і на основі її робити запит
+// if (locale === 'EN') {
+//     fetch() // виконуємо запит
+// }
+    // return new NextResponse('thisIsForBody', {headers: {'head': 'body'}})
+
+
+
+    // що робити, щоб url можна було використовувати різні?
+    // для цього працюємо if else
+    // є бібліотека Nemo яка це може замінити, але ще не дороблена, поки використовують if else
+    if (request.url.startsWith('http://localhost:3000/users')){
+        console.log('users store')
+    } else if (request.url.includes('auth')) {
+        console.log('auth')
+    }
+    else if (request.url.includes('3000')) {
+        console.log('3000')
+    }
+}
+
+
+// export const config = {
+//     matcher:'/auth'
+// }
